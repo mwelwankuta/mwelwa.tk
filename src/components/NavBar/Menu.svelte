d41d38304561f8136e77b4d87c63a2ad5cbfd5e7
Mwelwa Nkuta <mwelwankuta@gmail.com>
2021-12-05 13:34:10 +0200

    Add full screen mobile nav

--- src/components/NavBar/Menu.svelte
+++ src/components/NavBar/Menu.svelte
@@ -8,7 +8,7 @@
   const toggleMenu = () => dispatch("close-menu");
 </script>
 
-<ul use:links on:blur={toggleMenu} transition:slide>
+<ul use:links on:blur={toggleMenu} transition:slide={{duration:300}}>
   <li><a href="/" on:click={toggleMenu}>Home</a></li>
   <li><a href="/about" on:click={toggleMenu}>About</a></li>
   <li><a href="/dashboard" on:click={toggleMenu}>Dashboard</a></li>
@@ -19,6 +19,7 @@
   @import "../../variables.scss";
 
   ul {
+    height: 100vh;
     padding: 10px;
     top: 46px;
     background-color: white;
@@ -30,15 +31,17 @@
     border-radius: 5px;
     list-style-type: none;
     width: 100%;
+    padding-left: 54px;
 
     li {
       width: 100%;
-      margin-bottom: 10px;
+      margin-bottom: 24px;
       a {
         text-decoration: none;
         padding: 4px 8px;
         width: 100%;
         color: #333;
+        font-size: larger;
         &:hover {
           background-color: #f5f5f5;
           border-radius: 5px;
